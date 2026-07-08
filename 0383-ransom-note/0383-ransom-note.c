cca4bb4c55d7409e96fbea0c79a24b939f17e8b4
Monishka Yadav <monishka310307@gmail.com>
2026-07-08 17:22:03 +0530

    Time: 1 ms (53.15%), Space: 9.3 MB (89.54%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+
+bool canConstruct(char* ransomNote, char* magazine) {
+    int count[26] = {0};
+    for (int i = 0; magazine[i]; i++)
+        count[magazine[i] - 'a']++;
+    for (int i = 0; ransomNote[i]; i++)
+        if (--count[ransomNote[i] - 'a'] < 0) return false;
+    return true;
+}
