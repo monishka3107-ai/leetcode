5d71a59134351c213f616b0610d8c901ca35c62b
Monishka Yadav <monishka310307@gmail.com>
2026-07-08 17:10:59 +0530

    Time: 0 ms (100%), Space: 13.9 MB (93.82%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+
+int* findOrder(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize, int* returnSize) {
+    int n = numCourses, m = prerequisitesSize;
+    int *indeg = calloc(n, sizeof(int));
+    int *head = malloc(n * sizeof(int));
+    int *next = malloc((m > 0 ? m : 1) * sizeof(int));
+    int *to = malloc((m > 0 ? m : 1) * sizeof(int));
+    memset(head, -1, n * sizeof(int));
+    for (int i = 0; i < m; i++) {
+        int a = prerequisites[i][0], b = prerequisites[i][1]; // edge b -> a
+        to[i] = a;
+        next[i] = head[b];
+        head[b] = i;
+        indeg[a]++;
+    }
+    int *q = malloc(n * sizeof(int));
+    int h = 0, t = 0;
+    for (int i = 0; i < n; i++)
+        if (!indeg[i]) q[t++] = i;
+    while (h < t) {
+        int u = q[h++];
+        for (int e = head[u]; e != -1; e = next[e])
+            if (--indeg[to[e]] == 0) q[t++] = to[e];
+    }
+    free(indeg); free(head); free(next); free(to);
+    if (t == n) {
+        *returnSize = n;
+        return q; // queue order is a valid topological order
+    }
+    free(q);
+    *returnSize = 0;
+    return NULL;
+}
